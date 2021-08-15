6c943c1b57cbc81d8ef1a332fbedd503029f5fcb
prudvi-m <69066646+prudvi-m@users.noreply.github.com>
2021-08-15 18:00:29 +0530

    maintained flow

--- index.js
+++ index.js
@@ -2,19 +2,50 @@
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=geometry">
 let marker;
+let autocomplete;
+let map;
 
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: { lat: 34, lng: -40.605 },
+function initGoolgeMapAPI() {
+  searchBoxInit();
+  //mapInit();
+  //markerInit();
+}
+
+function searchBoxInit() {
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("txtAddress"),
+    {
+      types: ["geocode"],
+    }
+  );
+  google.maps.event.addListener(autocomplete, "place_changed", function () {
+    var near_place = autocomplete.getPlace();
+    console.log(near_place);
+    debugger;
+    var lat = near_place.geometry.location.lat();
+    var lng = near_place.geometry.location.lng();
+    mapInit(lat, lng);
+    markerInit(lat, lng);
   });
+}
+
+function mapInit(lat, lng) {
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4.5,
+    center: { lat: lat, lng: lng },
+    mode: "places",
+  });
+
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(
     document.getElementById("info")
   );
+}
+
+function markerInit(lat, lng) {
   marker = new google.maps.Marker({
     map,
     draggable: true,
-    position: { lat: 40.714, lng: -74.006 },
+    position: { lat: lat, lng: lng },
   });
   google.maps.event.addListener(marker, "position_changed", update);
   update();
